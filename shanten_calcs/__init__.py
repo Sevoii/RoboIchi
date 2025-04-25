8ac5030ead81f4d1d42d55ab8d7bd9159beaaa80
a <@>
2025-04-24 23:48:32 -0400

    move unneeded funcs out of agari file

--- shanten_calcs/__init__.py
+++ shanten_calcs/__init__.py
@@ -1,5 +1,31 @@
 from .shanten_calc import calc_all
+from .agari import get_tile14_and_key, get_hand_waits, get_agari_data
 
 
 def convert_t14_to_full(tiles: list[int]):
     return [tiles.count(i) for i in range(34)]
+
+
+def check_ankan_after_riichi(tehai: list[int], tile_id: int, strict: bool) -> bool:
+    if tehai[tile_id] != 4:
+        return False
+    elif tile_id >= 27:  # Honor Tile can always ankan
+        return True
+
+    tehai = tehai[:]  # Copying the list
+
+    tehai[tile_id] -= 1
+    waits = get_hand_waits(tehai)
+
+    tehai[tile_id] = 0
+
+    for i in waits:
+        tehai[i] += 1
+        t14, key = get_tile14_and_key(tehai)
+
+        if get_agari_data(key) is None:
+            return False
+
+        tehai[i] -= 1
+
+    return True
--- shanten_calcs/agari.py
+++ shanten_calcs/agari.py
@@ -156,29 +156,8 @@ def get_hand_waits(tehai: list[int]):
     return waits
 
 
-def check_ankan_after_riichi(tehai: list[int], tile_id: int, strict: bool) -> bool:
-    if tehai[tile_id] != 4:
-        return False
-    elif tile_id >= 27:  # Honor Tile can always ankan
-        return True
-
-    tehai = tehai[:]  # Copying the list
-
-    tehai[tile_id] -= 1
-    waits = get_hand_waits(tehai)
-
-    tehai[tile_id] = 0
-
-    for i in waits:
-        tehai[i] += 1
-        t14, key = get_tile14_and_key(tehai)
-
-        if key not in AGARI_TABLE:
-            return False
-
-        tehai[i] -= 1
-
-    return True
-
-
 AGARI_TABLE: dict[int, list['AgariData']] = load_table_from_gzip(__file__ + "/../data/agari.bin.gz")
+
+
+def get_agari_data(key: int):
+    return AGARI_TABLE.get(key)
